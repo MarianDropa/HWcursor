fb856dbbc99a81f2793c0132409a3a3f20801231
MarianDropa <90708734+MarianDropa@users.noreply.github.com>
2021-09-16 20:21:29 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+package org.cursor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte num1 = 50;
+        byte num2 = -3;
+        short num3 = -85;
+        short num4 = 20;
+        long num5 = 8552216L;
+        long num6 = 5200000L;
+        float num7 = 5.0008F;
+        float num8 = 2.001501F;
+        double num9 = 520.16161651;
+        double num10 = 250.51654166;
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1/num2);
+        System.out.println(num1*num2);
+        System.out.println(num3+num4);
+        System.out.println(num3-num4);
+        System.out.println(num3/num4);
+        System.out.println(num3*num4);
+        System.out.println(num5+num6);
+        System.out.println(num5-num6);
+        System.out.println(num5/num6);
+        System.out.println(num5*num6);
+        System.out.println(num7+num8);
+        System.out.println(num7-num8);
+        System.out.println(num7/num8);
+        System.out.println(num7*num8);
+        System.out.println(num9+num10);
+        System.out.println(num9-num10);
+        System.out.println(num9/num10);
+        System.out.println(num9*num10);
+        System.out.println(num1+num10);
+    }
+}
